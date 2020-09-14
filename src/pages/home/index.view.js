4290efd4999bafe982b33f79e29965d89575fe35
Syafrizal <rizal@payfazz.com>
2020-09-14 12:00:05 +0700

    change chart to area

--- src/pages/home/index.view.js
+++ src/pages/home/index.view.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Row, Col, Card, Statistic, Table } from 'antd'
 import {
-  LineChart,
-  Line,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
   Legend,
+  AreaChart,
+  Area,
 } from 'recharts'
 
 import MainView from '../../layout/main/index.view'
@@ -60,26 +60,32 @@ const HomeView = ({ latest, histories }) => (
         <Card>
           <p className="text-lg mb-6">IDR in Last 30 Days</p>
           <ResponsiveContainer width="100%" height={520}>
-            <LineChart data={histories}>
-              <CartesianGrid strokeDasharray="3 3" />
-              <XAxis dataKey="date" />
-              <YAxis domain={['dataMin-100', 'auto']} />
+            <AreaChart
+              data={histories.sort(
+                (a, b) =>
+                  parseInt(a.date.split('-').join('')) -
+                  parseInt(b.date.split('-').join('')),
+              )}>
+              <CartesianGrid vertical={false} strokeDasharray="3 3" />
+              <XAxis axisLine={false} dataKey="date" />
+              <YAxis axisLine={false} domain={['dataMin-100', 'auto']} />
               <Tooltip />
               <Legend />
-              <Line type="monotone" dataKey="IDR" stroke="#82ca9d" />
-            </LineChart>
+              <Area
+                type="monotone"
+                dataKey="IDR"
+                stroke="#82ca9d"
+                strokeWidth={3}
+                fillOpacity={0.05}
+              />
+            </AreaChart>
           </ResponsiveContainer>
         </Card>
       </Col>
       <Col span={12}>
         <Card className="h-full">
           <p className="text-lg mb-8">Last 30 Days Currencies</p>
-          <Table
-            rowKey="date"
-            // size="middle"
-            columns={columns}
-            dataSource={histories}
-          />
+          <Table rowKey="date" columns={columns} dataSource={histories} />
         </Card>
       </Col>
     </Row>
